b255e487e0ad2b155d764e947aaaed16f29d57aa
waltherj <waltherj@byu.net>
2017-03-29 02:15:22 -0600

    Fixing bug

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -7,7 +7,7 @@ app.controller('MainCtrl', ['$scope', '$http', function($scope, $http){
 	$scope.searchButtonClicked = function(word) {
 		$scope.searchedWord = word;
 console.log($scope.searchedWord);
-		$http.get("http://35.166.20.120:3005/getRestaurants?keyword=" + word).then(function(response){
+		$http.get("/getRestaurants?keyword=" + word).then(function(response){
 
 			$scope.results = response.data.results;
 
