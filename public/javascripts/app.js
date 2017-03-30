cb799bb2d8f671eeaea026067be6b7d301a26d9e
Konnor <konnorjp@gmail.com>
2017-03-30 04:51:58 +0000

    </body> </html>yApp">   <head>     <script src="https://www.gstatic.com/firebasejs/3.7.3/firebase.js"></script>     <script> // Initialize Firebase var config = {     apiKey: "AIzaSyDUN9fFfJWV78jxdfC2DCCjZl9DP2Z7t88",     authDomain: "chat-8ba4c.firebaseapp.com",     databaseURL: "https://chat-8ba4c.firebaseio.com",     storageBucket: "chat-8ba4c.appspot.com",     messagingSenderId: "628805433439"   }; firebase.initializeApp(config); </script>     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.8/angular.min.js"></script>     <script src="https://cdn.firebase.com/libs/angularfire/2.1.0/angularfire.min.js"></script>     <script src="javascripts/script.js"></script>   </head>
      <body>
    
      <div ng-controller="chatController">
      <form novalidate class="simple-form">
        Name: <input type="text" ng-model="user.name" /><br />
        Chat: <input type="text" ng-model="user.chat" /><br />
        <input type="submit" ng-click="update(user)" value="Save" />
      </form>
      <ul>
      <li ng-repeat='line in chats'>
      {{line.from}} - {{line.body}}
      </li>
      </ul>
    </div>
    
    </body>
    </html>yApp">
      <head>
        <script src="https://www.gstatic.com/firebasejs/3.7.3/firebase.js"></script>
        <script>
    // Initialize Firebase
    var config = {
        apiKey: "AIzaSyDUN9fFfJWV78jxdfC2DCCjZl9DP2Z7t88",
        authDomain: "chat-8ba4c.firebaseapp.com",
        databaseURL: "https://chat-8ba4c.firebaseio.com",
        storageBucket: "chat-8ba4c.appspot.com",
        messagingSenderId: "628805433439"
      };
    firebase.initializeApp(config);
    </script>
        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.8/angular.min.js"></script>
        <script src="https://cdn.firebase.com/libs/angularfire/2.1.0/angularfire.min.js"></script>
        <script src="javascripts/script.js"></script>
      </head>
    
      <body>
    
      <div ng-controller="chatController">
      <form novalidate class="simple-form">
        Name: <input type="text" ng-model="user.name" /><br />
        Chat: <input type="text" ng-model="user.chat" /><br />
        <input type="submit" ng-click="update(user)" value="Save" />
      </form>
      <ul>
      <li ng-repeat='line in chats'>
      {{line.from}} - {{line.body}}
      </li>
      </ul>
    </div>
    Updated routes

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,10 +3,11 @@ var app = angular.module('app', []);
 app.controller('MainCtrl', ['$scope', '$http', function($scope, $http){
 	$scope.searchedWord = "";
 	$scope.results = [];
+	$scope.favorites = [];
 
 	$scope.searchButtonClicked = function(word) {
 		$scope.searchedWord = word;
-console.log($scope.searchedWord);
+		console.log($scope.searchedWord);
 		$http.get("/getRestaurants?keyword=" + word).then(function(response){
 
 			$scope.results = response.data.results;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,28 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+
+/* Set up mongoose in order to connect to mongo database */
+var mongoose = require('mongoose'); //Adds mongoose as a usable dependency
+
+mongoose.connect('mongodb://localhost/restaurantsDB'); //Connects to a mongo database called "commentDB"
+
+var restaurantSchema = mongoose.Schema({ //Defines the Schema for this database
+Name: String,
+Rating: Number,
+PriceLevel: Number,
+Address: String
+});
+
+var Restaurant = mongoose.model('Restaurant', restaurantSchema); //Makes an object from that schema as a model
+
+var db = mongoose.connection; //Saves the connection as a variable to use
+db.on('error', console.error.bind(console, 'connection error:')); //Checks for connection errors
+db.once('open', function() { //Lets us know when we're connected
+console.log('Connected');
+});
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -9,5 +31,31 @@ router.get('/', function(req, res, next) {
 router.get('/getRestaurants', function(req, res, next) {
 	request("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=40.2518,-111.6493&radius=1500&type=restaurant&keyword=" + req.query.keyword + "&key=AIzaSyAFImmU7YcMWwyZIWJNG1ABoGfwjvRN4us").pipe(res);
 });
+
+router.get('/getFavorites', function(req, res, next) {
+	console.log("GET favorite route"
+	Restuarant.find(function(arr, favoriteList) {
+		if(err) return console.error(err)
+		else {
+			console.log(favoriteList);
+			res.json(favoriteList);
+		}
+	})
+});
+
+router.post('/favorite', function(req, res, next) {
+  console.log("POST favorite route"); //[1]
+  console.log(req.body); //[2]
+  var newfavorite = new Restaurant(req.body); //[3]
+  console.log(newfavorite); //[3]
+  newfavorite.save(function(err, post) { //[4]
+    if (err) return console.error(err);
+    console.log(post);
+    res.sendStatus(200);
+  });
+});
+
+
+
 
 module.exports = router;
