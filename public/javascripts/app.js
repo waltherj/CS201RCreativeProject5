dc08563826b4ac2d2e1781eab78b8078ac92c803
waltherj <waltherj@byu.net>
2017-03-30 14:36:20 -0600

    Working on somethings

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -8,10 +8,14 @@ app.controller('MainCtrl', ['$scope', '$http', function($scope, $http){
 	$scope.toggle = function(restaurant){
 		restaurant.toggle=!restaurant.toggle;
 		console.log(restaurant);
+		if (restaurant.toggle) {
 		var fav = {name: restaurant.name, rating: restaurant.rating, price_level: restaurant.price_level, vicinity: restaurant.vicinity};
 		$http.post('/favorite', fav).success(function(data) {
 			$scope.favorites.push(fav);
 		});
+		} else {
+		//$scope.removeFavoriteClicked(restaurant);
+		}
 	}
 
 	$scope.toggled = function(item) {
@@ -22,7 +26,7 @@ app.controller('MainCtrl', ['$scope', '$http', function($scope, $http){
 	$scope.searchButtonClicked = function(word) {
 		$scope.searchedWord = word;
 		console.log($scope.searchedWord);
-		$http.get("/getRestaurants?keyword=" + word).then(function(response){
+		$http.get("/restaurants?keyword=" + word).then(function(response){
 			$scope.results = response.data.results;
 
 		});
@@ -32,7 +36,7 @@ app.controller('MainCtrl', ['$scope', '$http', function($scope, $http){
 	
 	$scope.showFavorites = function() {
 		
-		$http.get("/getFavorites").then(function(response){
+		$http.get("/favorites").then(function(response){
 			$scope.favorites = response.data;
 		});
 	};
@@ -40,11 +44,13 @@ app.controller('MainCtrl', ['$scope', '$http', function($scope, $http){
 	$scope.showSearch = function() {
 		
 	}
-	$scope.removeFavoriteClicked = function(restaurant) {
-		$http.delete('/deleteFavorite', restaurant).success(function(data) {
-			var index = $scope.restaurants.indexOf(restuarant);
-			$scope.restaurants.splice(index, 1);
-		});
-	};
+	$scope.removeFavoriteClicked = function(favorite) {
+      $http.delete('/favorites/' + favorite._id )
+        .success(function(data){
+          console.log("delete worked");
+	  $scope.showFavorites();
+	  favorite.toggle = false;
+        });
+    };
 }]);
 
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-
-/* Set up mongoose in order to connect to mongo database */
-var mongoose = require('mongoose'); //Adds mongoose as a usable dependency
+var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/restaurantsDB'); //Connects to a mongo database called "commentDB"
 
-var restaurantSchema = mongoose.Schema({ //Defines the Schema for this database
+var FavoriteSchema = mongoose.Schema({ //Defines the Schema for this database
 name: String,
 rating: Number,
 price_level: Number,
 vicinity: String
 });
 
-var Restaurant = mongoose.model('Restaurant', restaurantSchema); //Makes an object from that schema as a model
+var Favorite = mongoose.model('Favorite', FavoriteSchema); //Makes an object from that schema as a model
 
 var db = mongoose.connection; //Saves the connection as a variable to use
 db.on('error', console.error.bind(console, 'connection error:')); //Checks for connection errors
@@ -28,38 +26,44 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/getRestaurants', function(req, res, next) {
+router.get('/restaurants', function(req, res, next) {
 	request("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=40.2518,-111.6493&radius=1500&type=restaurant&keyword=" + req.query.keyword + "&key=AIzaSyAFImmU7YcMWwyZIWJNG1ABoGfwjvRN4us").pipe(res);
 });
 
-router.get('/getFavorites', function(req, res, next) {
-	console.log("GET favorite route");
-	Restaurant.find(function(err, favoriteList) {
-		if(err) return console.error(err)
-		else {
-			res.json(favoriteList);
-		}
-	})
-});
-
-router.post('/favorite', function(req, res, next) {
-  console.log("POST favorite route"); //[1]
-  console.log(req.body); //[2]
-  var newfavorite = new Restaurant(req.body); //[3]
-  console.log(newfavorite); //[3]
-  newfavorite.save(function(err, post) { //[4]
-    if (err) return console.error(err);
-    console.log(post);
-    res.sendStatus(200);
+router.get('/favorites', function(req, res, next) {
+  Favorite.find(function(err, favorites){
+    if(err){ return next(err); }
+    res.json(favorites);
   });
 });
 
-router.delete('/removeFavorite', function(req, red, next) {
-  console.log("DELETE favorite route");
-  console.log(req.body);
-  req.restaurant.remove();
-  res.json(req.restaurant);
+router.param('favorite', function(req, res, next, id) {
+  var query = Favorite.findById(id);
+  query.exec(function (err, favorite){
+    if (err) { return next(err); }
+    if (!favorite) { return next(new Error("can't find favorite")); }
+    req.favorite = favorite;
+    return next();
+  });
 });
 
+router.get('/favorites/:favorite', function(req, res) {
+  res.json(req.favorite);
+});
+
+router.post('/favorite', function(req, res, next) {
+  var favorite = new Favorite(req.body);
+  favorite.save(function(err, favorite){
+    if(err){ return next(err); }
+    res.json(favorite);
+  });
+});
+
+router.delete('/favorites/:favorite', function(req, res) {
+  console.log("in Delete");
+  req.favorite.remove();
+  res.json(req.favorite);
+});
 
 module.exports = router;
+
