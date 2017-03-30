25c1c07213497e74ea2ff4f398e468f968469389
Konnor <konnorjp@gmail.com>
2017-03-30 05:31:16 +0000

    HTML and Javascript added to add favorites

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,6 +4,7 @@ app.controller('MainCtrl', ['$scope', '$http', function($scope, $http){
 	$scope.searchedWord = "";
 	$scope.results = [];
 	$scope.favorites = [];
+	$scope.addFavText = "Add to Favorites";
 
 	$scope.searchButtonClicked = function(word) {
 		$scope.searchedWord = word;
@@ -14,6 +15,19 @@ app.controller('MainCtrl', ['$scope', '$http', function($scope, $http){
 
 		});
 	};
+	
 
+	$scope.addFavoriteClicked = function(restaurant) {
+		$scope.addFavText = "Favorite";
+		$http.post('/favorite', restaurant).success(function(data) {
+			$scope.favorites.push(restuarant);
+		});
+	};
+	
+	$scope.showFavorites = function() {
+		$http.get("/getFavorites").then(function(response){
+			$scope.results = response.data.results;
+		});
+	};
 }]);
 
